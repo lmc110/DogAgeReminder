85543b586e44d3ffc0cfa0c3feffa777cf5872a8
luism <luismcampos32@gmail.com>
2018-07-18 02:15:16 -0500

    created template files and calculate age in python file

--- DogAgeReminder.py
+++ DogAgeReminder.py
@@ -1,11 +1,49 @@
-from flask import Flask
+import flask
+from datetime import datetime, date
+from dateutil import relativedelta
+import math
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return flask.render_template('index.html')
+
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    form_date = flask.request.form['pet-age']
+    pet_age = form_date.replace('-', ' ')
+    date_of_birth = datetime.strptime(pet_age, "%Y %m %d")
+    total_age = calculate_total(date_of_birth)
+    total_days = calculate_days(date_of_birth)
+    total_weeks = calculate_weeks(date_of_birth)
+    return flask.render_template('age.html', total_age=total_age, total_days=total_days, total_weeks=total_weeks)
+
+
+def calculate_total(born):
+    today = date.today()
+    r = relativedelta.relativedelta(today, born)
+    #print("Total age: " + str(r.years) + " years " + str(r.months) + " months " + str(r.days) + " days")
+    return r
+
+
+def calculate_days(born):
+    # Calculate total number of days born
+    days = date.today()-date(born.year, born.month, born.day)
+    #print("Days: " + str(days.days))
+    return days
+
+
+def calculate_weeks(born):
+    # Calculate total number of weeks
+    days = date.today() - date(born.year, born.month, born.day)
+    weeks = math.floor(days.days / 7)
+    #print("Weeks: " + str(weeks))
+    return weeks
 
 
 if __name__ == '__main__':
